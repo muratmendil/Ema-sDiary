893659b8d3719d3466d7ed8f3a3ac6505ddf6237
Emilin <dadie.emilin@gmail.com>
2018-09-30 22:41:17 +0200

    fixed controller for jenkins 2

--- EmaFront/src/main/java/com/formation/controller/LoginController.java
+++ EmaFront/src/main/java/com/formation/controller/LoginController.java
@@ -6,7 +6,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
