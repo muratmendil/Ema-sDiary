24b5a1ff2118b679bd7d36582b84af204bb81da9
Emilin <dadie.emilin@gmail.com>
2018-12-06 19:19:36 +0100

    update webservices

--- WebServices/src/main/java/com/formation/api/EmaAPIService.java
+++ WebServices/src/main/java/com/formation/api/EmaAPIService.java
@@ -13,6 +13,8 @@ import com.formation.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.GenericEntity;
@@ -119,6 +121,14 @@ public class EmaAPIService {
 		} catch (ErrorExeption e) {
 			return Response.status(Response.Status.UNAUTHORIZED).entity(e.getExeptionMessage()).build();
 		} 
+	 }
+	
+	@DELETE
+	@Path("/deleteTask/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	 public Response deleteTask(@PathParam("id") int id){
+			 Facade.getInstance().getTaskService().deleteTask(id);
+			 return Response.ok().entity(true).build();
 	 }
 	
 	@POST
