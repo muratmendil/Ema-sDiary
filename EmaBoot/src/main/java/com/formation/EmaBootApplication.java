c05f40299f62e363f529067a5db65fa8406af873
emilindadie <dadie.emilin@gmail.com>
2019-03-27 10:31:17 +0100

    cors filter created

--- EmaBoot/src/main/java/com/formation/EmaBootApplication.java
+++ EmaBoot/src/main/java/com/formation/EmaBootApplication.java
@@ -2,6 +2,10 @@ package com.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EmaBootApplication {
@@ -9,5 +13,14 @@ public class EmaBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmaBootApplication.class, args);
 	}
-
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/webservices/api/ema/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
